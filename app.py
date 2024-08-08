bb9a1aee2d0dee9375f35ab21726d9a1620ae2b6
Stan Davies <stan07davies@gmail.com>
2024-08-08 19:11:36 +0100

    updated tablename in DB model

--- app.py
+++ app.py
@@ -9,14 +9,15 @@ app.config.from_object(env_config)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://uadjvu5175esqk:pa922e6eb711f6b69b1a478043cac3d71f7c71f94afe988790405a864c3dfa37f@c97r84s7psuajm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dbdl7l466g8acq'
 db = SQLAlchemy(app)
 
-class MyModel(db.Model):
+class Player(db.Model):
+    __tablename__ =  "players"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    value = db.Column(db.String(120))
+    name = db.Column(db.String(100))
+    value = db.Column(db.Integer, default=0)
 
 @app.route("/")
 def index():
-    results = MyModel.query.all()
+    results = Player.query.all()
     return render_template('index.html', results=results)
     secret_key = app.config.get("SECRET_KEY")
     return f"The configured secret key is {secret_key}. TEST TEST TEST aaaa"
